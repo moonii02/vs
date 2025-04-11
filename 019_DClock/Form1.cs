ef0cfcdb99169e846c7d66b96090d966547d0c69
moonii02 <sm021213@naver.com>
2025-04-11 18:46:10 +0900

    Add files via upload

---
+++ 019_DClock/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _019_DClock
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Tick += Timer1_Tick;
+
+            lblDate.Font = new Font("맑은 고딕", 16, FontStyle.Bold);
+            lblDate.ForeColor = Color.BlueViolet;
+            lblTime.Font = new Font("맑은 고딕", 32, FontStyle.Bold);
+            lblTime.ForeColor = Color.Aqua;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            lblDate.Text = DateTime.Now.ToString("yyyy년 MM월 dd일");
+            lblTime.Text = DateTime.Now.ToString("tt h:mm:ss");
+
+            lblDate.Location = new Point(ClientSize.Width / 2 - lblDate.Width / 2,
+                ClientSize.Height/2 - lblDate.Height/2 - 30);
+            lblTime.Location = new Point(ClientSize.Width / 2 - lblTime.Width / 2,
+                ClientSize.Height / 2 - lblTime.Height / 2 + 20);
+        }
+
+        private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ 023_StaticMethod/Date.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _023_StaticMethod
+{
+    internal class Date
+    {
+        public int year, month, day;
+
+        public static bool IsLeapYear(int y)
+        {   
+            // 윤년은 4로 나누어지고 100으로는 나누어지지 않는 해
+            // 그런데 400으로 나누어지면 윤년
+            return (y % 4 == 0 && y % 100 != 0) || y % 400 == 0;
+        }
+
+        static int[] days = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+
+        public int DayOfYear()
+        {
+            //if (month > 2 && IsLeapYear(year)) 
+            //    return days[month - 1] + day;
+            //else
+            //    return days[month - 1] + day;
+
+            return days[month - 1] + day
+                + (month > 2 && IsLeapYear(year) ? 1 : 0);
+        }
+    }
+}
