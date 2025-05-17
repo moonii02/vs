4074208fcddc9e166c7fa543f7ee1be34ff243a1
moonii02 <sm021213@naver.com>
2025-05-17 16:28:22 +0900

    Add files via upload

---
+++ 031_Property/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _031_Property
+{
+    class Rect 
+    {
+        public int Width { get; set; }
+        private int height;
+
+        public int Height
+        {
+            get { return height; }
+            set { if (value >= 0) height = value; }
+        }
+
+    }
+    class Rectangle
+    {
+        private int width; int height;
+        public void SetWidth(int w) { width = w; }
+        public int GetWidth() { return width; }
+        public void SetHeight(int h) { height = h; }
+        public int GetHeight() { return height; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            Rect r1 = new Rect():
+            r1.
+        }
+    }
+}
---
+++ 032_HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _032_HelloWorld
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //    MessageBox.Show("Text Clicked", "String Msg");
+
+            if (grid1.Background == Brushes.DarkViolet)
+                grid1.Background = Brushes.Aqua;
+            else if (grid1.Background == Brushes.Aqua)
+                grid1.Background = Brushes.Red;
+            else if (grid1.Background == Brushes.Red)
+                grid1.Background = Brushes.DarkSeaGreen;
+            else
+                grid1.Background = Brushes.DarkViolet;
+        }
+    }
+}
